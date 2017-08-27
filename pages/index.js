69e42372f925dc0db9fe5eaa4c2efc7ecc10a16b
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2017-08-27 11:08:31 +0700

    Add basic component with absolute import configured

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,13 @@
-const Root = () => <h1>Pronto Racing</h1>
+import Link from 'next/link'
+import Test from 'components/Test'
+
+const Root = () =>
+  <div>
+    <h1>Pronto Racing</h1>
+    <Link href='/play'>
+      <button>Start racing</button>
+    </Link>
+    <Test />
+  </div>
 
 export default Root
