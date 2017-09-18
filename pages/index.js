39abbb5e5b4f6c77c8e0733f0babfd39ceaf8050
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2017-09-18 23:28:55 +0700

    Add emotion

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
 import Link from 'next/link'
+import { hydrate, injectGlobal } from 'emotion'
+
 import Test from 'components/Test'
+
+// Adds server generated styles to emotion cache.
+if (typeof window !== 'undefined') {
+  hydrate(window.__NEXT_DATA__.ids)
+}
 
 const Root = () =>
   <div>
@@ -9,5 +16,12 @@ const Root = () =>
     </Link>
     <Test />
   </div>
+
+injectGlobal`
+  html, body {
+    color: #333;
+    font-size: 16px;
+  }
+`
 
 export default Root
