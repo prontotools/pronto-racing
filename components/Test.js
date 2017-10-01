77ee3bbca07cd57bdfe2ee22044c69f867e72667
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2017-10-01 22:40:58 +0700

    Upgrade node package versions to latest
    Except eslint-plugin-jsx-a11y which has to be version 5 because
    eslint-plugin-react-app use a rule that only exist in version 5.

--- components/Test.js
+++ components/Test.js
@@ -1,1 +1,9 @@
-export default () => <span>Test</span>
+import React from 'react'
+
+class Test extends React.Component {
+  render () {
+    return [<div key='1'>Array</div>, <div key='2'>Because we're cool</div>]
+  }
+}
+
+export default Test
