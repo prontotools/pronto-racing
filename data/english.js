6dbce90e91921540499cfb6c9b918e7f3eb5271e
jiippoozz <32768912+jiippoozz@users.noreply.github.com>
2017-10-14 20:04:16 +0700

    Add texts to data

--- data/english.js
+++ data/english.js
@@ -7,4 +7,21 @@ export const texts = {
     text:
       "It's supercalifragilisticexpialidocious. Even though the sound of it is something quite atrocious. If you say it loud enough, you'll always sound precocious. Supercalifragilisticexpialidocious!"
   }
+  3: {
+    text:
+      "It's Hacktoberfest 2017! Let’s contribute to open source! This round of CodeClub will be Hacktoberfest-themed!! It will take place on October 14th at Pronto Tools."
+  }
+  4:{
+    text:
+    "About CodeClub, we don't talk about CodeClub"
+  }
+  5: {
+    text:
+      "'There are two ways of constructing a software design. One way is to make it so simple that there are obviously no deficiencies. nd the other way is to make it so complicated that there are no obvious deficiencies.' - C.A.R. Hoare'"
+  }
+  6: {
+    text:
+    "'Most good programmers do programming not because they expect to get paid or get adulation by the public, but because it is fun to program.' - Linus Torvalds"
+  }
+  }
 }
